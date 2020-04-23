89c7c41a85db01d3d04fb60844c1021f09e0a92c
Vedadnya Jadhav <vedadnya@gmail.com>
2020-04-22 21:57:21 -0400

    base

---
+++ firebase-auth/src/firebase/FirebaseFunctions.js
@@ -0,0 +1,60 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+let db  = firebase.firestore();
+
+async function doCreateUserWithEmailAndPassword(email, password, firstName , lastName) {
+	await firebase.auth().createUserWithEmailAndPassword(email, password)
+	var user = firebase.auth().currentUser;
+	let data = {
+		firstName: firstName,
+		lastName : lastName,
+		email : email
+	  };
+	  let setDoc = await db.collection('users').doc(user.uid).set(data);	
+	  console.log(setDoc)	  
+	firebase.auth().currentUser.updateProfile({ displayName: String(firstName)+" "+String(lastName) });
+}
+
+async function doChangePassword(email, oldPassword, newPassword) {
+	let credential = firebase.auth.EmailAuthProvider.credential(email, oldPassword);
+	await firebase.auth().currentUser.reauthenticateWithCredential(credential);
+	await firebase.auth().currentUser.updatePassword(newPassword);
+	await doSignOut();
+}
+
+async function doSignInWithEmailAndPassword(email, password) {
+	await firebase.auth().signInWithEmailAndPassword(email, password);
+}
+
+async function doSocialSignIn(provider) {
+	let socialProvider = null;
+	if (provider === 'google') {
+		socialProvider = new firebase.auth.GoogleAuthProvider();
+	} else if (provider === 'facebook') {
+		socialProvider = new firebase.auth.FacebookAuthProvider();
+	}
+	await firebase.auth().signInWithPopup(socialProvider);
+}
+
+async function doPasswordReset(email) {
+	await firebase.auth().sendPasswordResetEmail(email);
+}
+
+async function doPasswordUpdate(password) {
+	await firebase.auth().updatePassword(password);
+}
+
+async function doSignOut() {
+	await firebase.auth().signOut();
+}
+
+export {
+	doCreateUserWithEmailAndPassword,
+	doSocialSignIn,
+	doSignInWithEmailAndPassword,
+	doPasswordReset,
+	doPasswordUpdate,
+	doSignOut,
+	doChangePassword
+};
