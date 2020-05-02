55403eb667092260dfc3a375cb31db875d31da95
Vedadnya Jadhav <vedadnya@gmail.com>
2020-05-02 14:59:42 -0400

    login signup modal

--- firebase-auth/src/components/App.js
+++ firebase-auth/src/components/App.js
@@ -6,8 +6,6 @@ import Home from './Home';
 import Landing from './Landing';
 import University from './University';
 import Navigation from './Navigation';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
 import { AuthProvider } from '../firebase/Auth';
 import PrivateRoute from './PrivateRoute';
 function App() {
