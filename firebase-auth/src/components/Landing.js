3d4bb7259f55561eeb0a049b4c13f19c19e87957
Vedadnya Jadhav <vedadnya@gmail.com>
2020-05-01 21:52:14 -0400

    landing skeleton

--- firebase-auth/src/components/Landing.js
+++ firebase-auth/src/components/Landing.js
@@ -1,17 +1,17 @@
-import React, {useContext, useState, useEffect } from 'react';
+import React, {useContext , useState, useEffect } from 'react';
 import '../App.css';
-import { Redirect } from 'react-router-dom';
 import { AuthContext } from "../firebase/Auth";
+import { Redirect } from 'react-router-dom';
 import 'firebase/firestore';
-import {addPosts, getUser , getAllColleges} from '../firebase/FirestoreFunctions';
+import { getAllColleges , getAllPosts} from '../firebase/FirestoreFunctions';
 
 
 
 
-function Landing() {	
+function Landing() {
 	const {currentUser} = useContext(AuthContext)
-	const [user, setUser] = useState();
 	const [colleges, setCollege] = useState();
+	const [allPosts, setallPosts] = useState();
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
@@ -19,28 +19,17 @@ function Landing() {
 		try{
 			let collegeList = await getAllColleges();
 			console.log(collegeList)
-			let u = await getUser(currentUser.uid);
+			let allPost = await getAllPosts()
 			setLoading(false)
-			setUser(u);
 			setCollege(collegeList)
+			setallPosts(allPost)
 		}catch(e){
 			console.log(e)
 	}
 }
 		getData();
-	}, [currentUser])
+	}, [])
 
-	const handlePosts = async (event) => {
-			event.preventDefault();
-			let { title, value ,  description, category } = event.target.elements;
-			let post ={title:title.value, authorId:currentUser.uid, value:value.value ,description:description.value, category: category.value};
-	
-			try {
-				await addPosts(currentUser.uid, post);
-			} catch (error) {
-				alert(error);
-			}
-		};
 
 	if(currentUser)
 	{
@@ -52,85 +41,20 @@ function Landing() {
 			return (
 				<div className='Landing'>
 					<h2>This is the Landing page</h2>
-					{user ? (<p>{user.firstName}  {user.lastName}</p>) : (<p>NOT GETTING USER DATA</p>)}
-					{user && user ? (<p>{user.posts.map((item) => {
-						return (<div>
-									<p>{item.title} : {item.value}</p>
-									<p>{item.description}</p>
-								</div>)
-					})}</p>) : (<p>NOT GETTING USER DATA</p>)}
-
+					<p>COLLEGES: </p>
 					{colleges && colleges ? (<p>{colleges.map((item) => {
 						return (<div>
 									<p>{item.name}</p>
 								</div>)
 					})}</p>) : (<p>NOT GETTING College DATA</p>)}
-					<p> ADD YOUR POSTS BELOW </p>
-					<form onSubmit={handlePosts}>
-							<div className='form-group'>
-								<label>
-									Title:
-									<input
-										className='form-control'
-										name='title'
-										id='title'
-										type='text'
-										placeholder='Title'
-										required
-										/>
-								</label>
-								<label>
-									Value:
-									<input
-										className='form-control'
-										name='value'
-										id='value'
-										type='textarea'
-										placeholder='Value'
-										required
-									/>
-								</label>
-								<label>
-									Description:
-									<input
-										className='form-control'
-										name='description'
-										id='description'
-										type='textarea'
-										placeholder='Description'
-										required
-									/>
-								</label>
-								<select id='category' name='category'>
-								{/* {category.map(item => (
-									<option key={item.id} value={item.name}>
-										{item.name}
-									</option>
-								))} */}
-								<option key='rent' value='rent'>
-									RENT
-								</option>
-								<option key='houses' value='houses'>
-									HOUSES
-								</option>
-								<option key='groceries' value='groceries'>
-									GROCERIES
-								</option>
-								<option key='transport' value='transport'>
-									TRANSPORT
-								</option>
-								</select>
-							</div>
-							<button type='submit'> Post </button>
-						</form> 
-						{/* <select id='college' name='college'>
-								{colleges.map(item => (
-									<option key={item.uid} value={item.name}>
-										{item.name}
-									</option>
-								))}	
-								</select> */}
-
+					
+					<p>POSTS: </p>
+					{allPosts && allPosts ? (<p>{allPosts.map((item) => {
+						return (<div>
+									<p>{item.category}</p>
+								</div>)
+					})}</p>) : (<p>NOT GETTING Post DATA</p>)}
+					<p> Login to post anything</p>
 				</div>
 			)}
 			else{
