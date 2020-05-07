f92baf2ff163049eff007cc91b374a8e177cabd6
Vedadnya Jadhav <vedadnya@gmail.com>
2020-05-07 15:28:47 -0400

    bootstrap modal partial

--- firebase-auth/src/components/Navigation.js
+++ firebase-auth/src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React, { useContext , useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import Modal from 'react-modal'
+//import Modal from 'react-modal'
+import {Button, Modal} from 'react-bootstrap'
 import { AuthContext } from '../firebase/Auth';
 import SignOutButton from './SignOut';
 import SignIn from './SignIn';
@@ -29,13 +30,11 @@ const NavigationAuth = () => {
   <div class="collapse navbar-collapse collapse w-100" id="navbarTogglerDemo02">
   <ul class="navbar-nav mr-auto mt-2 mt-lg-0 w-100 justify-content-center">
       <li class="nav-item active">
-
 	  <NavLink exact to='/home' class = "nav-link">
 						Home
 					</NavLink>
-
-        {/* <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> */}
       </li>
+
 	  <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Universities
@@ -68,11 +67,7 @@ const NavigationAuth = () => {
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
-
-
     </ul>
-
-
 	<ul class="navbar-nav mt-2 mt-lg-0  ml-auto w-100 justify-content-end ">
 
 		<li class = "nav-item">
@@ -83,37 +78,20 @@ const NavigationAuth = () => {
 		<li class = "nav-item">
 		<SignOutButton />
 		</li>
-
-
 		</ul>
-
-	
   </div>
   </div>
 </nav>
-
-			/* <ul>
-				<li>
-					<NavLink exact to='/home' activeClassName='active'>
-						Home
-					</NavLink>
-				</li>
-				<li>
-					<NavLink exact to='/profile' activeClassName='active'>
-						Account
-					</NavLink>
-				</li>
-				<li>
-					<SignOutButton />
-				</li>
-			</ul>
-		</nav> */
 	);
 };
 
 const NavigationNonAuth = () => {
-	const [modal, setModal] = useState();
-	const [button,setButton] = useState("Signup");
+	const [logSign,setlogSign] = useState("Signup");
+	const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	// const [modal, setModal] = useState();
+	// const [button,setButton] = useState("Signup");
  	return (
 	
 
@@ -125,39 +103,11 @@ const NavigationNonAuth = () => {
 		
 		<div class = "container">
 		<a class="navbar-brand" href="">xPense</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-	  <ul>
-
-	  </ul>
-	  <ul class="navbar-nav mt-2 mt-lg-0  ml-auto w-100 justify-content-end ">
-
-				<button onClick={()=> setModal(true)}>Login/SignUp</button>
-						<Modal className = "container modalContent" isOpen={modal} onRequestClose = {()=> setModal(false)}>
-							
-							<button onClick={()=> button==="Login"? setButton("Signup") : setButton("Login")}>{button}</button>
-							{button === "Login"?<SignUp></SignUp> : <SignIn></SignIn>}
-							<br></br>
-							<button onClick={()=> setModal(false)}>Close</button>
-							
-						
-            			</Modal>
-	
-	</ul>
-
-	
-  </div>
-
-		</div>
-  
-</nav>
-
-
-
-/* 
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 			<ul>
 				<li>
 					<NavLink exact to='/' activeClassName='active'>
@@ -165,15 +115,38 @@ const NavigationNonAuth = () => {
 					</NavLink>
 				</li>
 				<li>
+					<Button variant="primary" onClick={handleShow}>
+						Login/SignUp
+					</Button>
+
+					<Modal show={show} onHide={handleClose} animation={false}>
+						<Modal.Header closeButton>
+							<Modal.Title>{logSign}</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>{logSign === "Signup"?<SignUp/>:<SignIn/>}</Modal.Body>
+						<Modal.Footer>
+						<Button variant="secondary" onClick={handleClose}>
+							Close
+						</Button>
+						{/* <Button variant="primary" onClick={logSign==="Login"? setlogSign("Signup") : setlogSign("Login")}>
+							Create Account
+						</Button> */}
+						</Modal.Footer>
+					</Modal>
+				</li>
+				{/* <li>
 				<button onClick={()=> setModal(true)}>Login/SignUp</button>
 						<Modal isOpen={modal} onRequestClose = {()=> setModal(false)}>
 							<button onClick={()=> button==="Login"? setButton("Signup") : setButton("Login")}>{button}</button>
 							{button === "Login"?<SignUp></SignUp> : <SignIn></SignIn>}
 							<button onClick={()=> setModal(false)}>Close</button>
             			</Modal>
-				</li>
+				</li> */}
+			</ul>
+  				</div>
+		</div>
+</nav>
 
-			</ul> */
 		
 	);
 };
