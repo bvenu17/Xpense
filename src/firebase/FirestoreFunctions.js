289743c85967f6f65cef921baa3b09a77d770af2
Venugopal Balaji <37976236+bvenu17@users.noreply.github.com>
2020-05-16 03:25:57 -0400

    chat db update

--- src/firebase/FirestoreFunctions.js
+++ src/firebase/FirestoreFunctions.js
@@ -195,8 +195,8 @@ async function addCommentToPost(postId, userName, commentText) {
 async function addChat(chatObject) {
   //func to add post to db
   // let chatData = { name: userName, message: chatObject }
-  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
-  chatObject.createdAt = timestamp();
+  //const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+  chatObject.createdAt = new Date();
 //  await db.collection("chats").add(chatObject)
     await db.collection("chats").doc('allChats').update({
       chatMessage:firebase.firestore.FieldValue.arrayUnion(chatObject)
